d9bd3093fcaa9457e571d34891dfdbf20f7c7cf0
Luca Mannella <luca.mannella@studenti.polito.it>
2017-08-04 19:09:45 +0200

    First prototype

--- Tic-Tac-Toe_Neural-Network/NeuralNetwork.m
+++ Tic-Tac-Toe_Neural-Network/NeuralNetwork.m
@@ -10,3 +10,34 @@ disp(' \_\ \/ \___|\__,_|_|  \__,_|_| \_\ \/ \___|\__| \_/\_/ \___/|_|  |_|\_\ '
 disp('  Developed by Luca Mannella - August 2017                     (!) v0.1  ')
 disp('                                                                         ')
 
+load('..\Tic-Tac-Toe_Minimax\nn_input.txt')
+load('..\Tic-Tac-Toe_Minimax\nn_output.txt')
+
+inputs = nn_input';
+targets = nn_output';
+
+% Create a Pattern Recognition Network
+hiddenLayerSize = 40;
+net = patternnet(hiddenLayerSize);
+
+% Setup Division of Data for Training, Validation, Testing
+net.divideParam.trainRatio = 50/100;
+net.divideParam.valRatio = 10/100;
+net.divideParam.testRatio = 40/100;
+
+% Train the Network
+[net, tr] = train(net, inputs, targets);
+
+% Test the Network
+outputs = net(inputs);
+errors = gsubtract(targets, outputs);
+performance = perform(net, targets, outputs)
+
+% View the Network
+view(net)
+
+% Plots
+%figure, plotperform(tr)
+%figure, plottrainstate(tr)
+%figure, plotconfusion(targets,outputs)
+%figure, ploterrhist(errors)
