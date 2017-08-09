7cd647ffcd8764d075609429e225af72bc32cd99
Luca Mannella <luca.mannella@studenti.polito.it>
2017-08-09 17:36:09 +0200

    The NeuralNetwork script is now a function.

--- Tic-Tac-Toe_Neural-Network/NeuralNetwork.m
+++ Tic-Tac-Toe_Neural-Network/NeuralNetwork.m
@@ -1,43 +1,37 @@
-clear all
-close all
-clc
+function [outputs, errors, performance, nRightValues] = NeuralNetwork(inputs, targets, hiddenLayerSize, trainingPerc, validationPerc, testingPerc, plotting)
 
-disp('      __                     _       __     _                      _     ')
-disp('   /\ \ \___ _   _ _ __ __ _| |   /\ \ \___| |___      _____  _ __| | __ ')
-disp('  /  \/ / _ \ | | |  __/ _` | |  /  \/ / _ \ __\ \ /\ / / _ \|  __| |/ / ')
-disp(' / /\  /  __/ |_| | | | (_| | | / /\  /  __/ |_ \ V  V / (_) | |  |   <  ')
-disp(' \_\ \/ \___|\__,_|_|  \__,_|_| \_\ \/ \___|\__| \_/\_/ \___/|_|  |_|\_\ ')
-disp('  Developed by Luca Mannella - August 2017                     (!) v0.1  ')
-disp('                                                                         ')
+    %% Create a Pattern Recognition Network with hiddenLayerSize neurons
+    nRightValues = 0;
+    net = patternnet(hiddenLayerSize);
 
-load('..\Tic-Tac-Toe_Minimax\nn_input.txt')
-load('..\Tic-Tac-Toe_Minimax\nn_output.txt')
+    % Setup Division of Data for Training, Validation, Testing
+    net.divideParam.trainRatio = trainingPerc;
+    net.divideParam.valRatio = validationPerc;
+    net.divideParam.testRatio = testingPerc;
 
-inputs = nn_input';
-targets = nn_output';
+    % Train the Network
+    [net, tr] = train(net, inputs, targets);
 
-% Create a Pattern Recognition Network
-hiddenLayerSize = 40;
-net = patternnet(hiddenLayerSize);
+    % Test the Network
+    outputs = net(inputs);
+    errors = gsubtract(targets, outputs);
+    performance = perform(net, targets, outputs);
 
-% Setup Division of Data for Training, Validation, Testing
-net.divideParam.trainRatio = 50/100;
-net.divideParam.valRatio = 10/100;
-net.divideParam.testRatio = 40/100;
+    outputs = round(outputs);
+    for i=1:length(targets)
+        if targets(1,i) == outputs(1,i)
+            nRightValues = nRightValues + 1;
+        end
+    end
+    
+    if plotting
+        % View the Network
+        view(net)
 
-% Train the Network
-[net, tr] = train(net, inputs, targets);
-
-% Test the Network
-outputs = net(inputs);
-errors = gsubtract(targets, outputs);
-performance = perform(net, targets, outputs)
-
-% View the Network
-view(net)
-
-% Plots
-%figure, plotperform(tr)
-%figure, plottrainstate(tr)
-%figure, plotconfusion(targets,outputs)
-%figure, ploterrhist(errors)
+        % Plots
+        figure, plotperform(tr)
+        figure, plottrainstate(tr)
+        figure, plotconfusion(targets, outputs)
+        figure, ploterrhist(errors)
+    end
+end
